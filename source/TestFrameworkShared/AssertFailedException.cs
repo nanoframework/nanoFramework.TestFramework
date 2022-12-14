26ba7c03a6bdbc42ce5cceb1f6b1b81548ab3031
José Simões <jose.simoes@eclo.solutions>
2022-12-14 15:15:53 +0000

    Rework Asserts
    - Rename several asserts to match with official MS Test framework.
    - Add obsolete attribute to the old ones.
    - Rename several parameters to match official API.
    - Add missing parameters to Intellisense comments.
    - Rework most of Intellisense comments to match official docs.
    - Add new class CollectionAsserts.

---
+++ source/TestFrameworkShared/AssertFailedException.cs
@@ -0,0 +1,41 @@
+﻿////
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+////
+
+using System;
+
+namespace TestFrameworkShared
+{
+    /// <summary>
+    /// <see cref="AssertFailedException"/> class. Used to indicate failure for a test case.
+    /// </summary>
+    public class AssertFailedException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssertFailedException"/> class.
+        /// </summary>
+        /// <param name="msg">The message.</param>
+        /// <param name="ex">The exception.</param>
+        public AssertFailedException(string msg, Exception ex)
+               : base(msg, ex)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssertFailedException"/> class.
+        /// </summary>
+        /// <param name="msg">The message.</param>
+        public AssertFailedException(string msg)
+            : base(msg)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssertFailedException"/> class.
+        /// </summary>
+        public AssertFailedException()
+        {
+        }
+    }
+}
