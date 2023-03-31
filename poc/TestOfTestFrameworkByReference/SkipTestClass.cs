e5b5e313f53a840c311fefcbdace130a44165bb0
José Simões <jose.simoes@eclo.solutions>
2023-03-31 13:52:02 +0100

    Update PoC projects (#199)
    ***NO_CI***

--- poc/TestOfTestFrameworkByReference/SkipTestClass.cs
+++ poc/TestOfTestFrameworkByReference/SkipTestClass.cs
@@ -5,7 +5,6 @@
 //
 
 using nanoFramework.TestFramework;
-using System;
 using System.Diagnostics;
 
 namespace NFUnitTest
@@ -16,36 +15,50 @@ namespace NFUnitTest
         [Setup]
         public void SetupMethodToSkip()
         {
-            Debug.WriteLine("Skippîng all the other methods");
+            Debug.WriteLine("Skipping all the other methods");
             Assert.SkipTest("None of the other methods should be tested, they should all be skipped.");
         }
 
         [TestMethod]
         public void TestMothdWhichShouldSkip()
-        { }
+        {
+            // Method intentionally left empty.
+        }
 
         [TestMethod]
         public void TestMothdWhichShouldSkip2()
-        { }
+        {
+            // Method intentionally left empty.
+        }
 
         [TestMethod]
         public void TestMothdWhichShouldSkip3()
-        { }
+        {
+            // Method intentionally left empty.
+        }
 
         [TestMethod]
         public void TestMothdWhichShouldSkip4()
-        { }
+        {
+            // Method intentionally left empty.
+        }
 
         [TestMethod]
         public void TestMothdWhichShouldSkip5()
-        { }
+        {
+            // Method intentionally left empty.
+        }
 
         [TestMethod]
         public void TestMothdWhichShouldSkip6()
-        { }
+        {
+            // Method intentionally left empty.
+        }
 
         [Cleanup]
         public void CleanUpMethodSkip()
-        { }
+        {
+            // Method intentionally left empty.
+        }
     }
 }
