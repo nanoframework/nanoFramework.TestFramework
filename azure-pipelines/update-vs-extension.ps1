3751a2213484c87f59c535421301a34d21257f4c
José Simões <jose.simoes@eclo.solutions>
2026-05-07 10:05:13 +0100

    Work CI-CD
    - Improve update PS1 to properly deal with preview packages and checking out appropriate branch.
    
    ***NO_CI***

--- azure-pipelines/update-vs-extension.ps1
+++ azure-pipelines/update-vs-extension.ps1
@@ -42,12 +42,23 @@ $commitMessage = ""
 $prTitle = ""
 $newBranchName = "develop-nfbot/update-dependencies/" + [guid]::NewGuid().ToString()
 $packageName = "nanoframework.testframework"
-$repoMainBranch = "main"
+$repoBranch = "main"
 
 # get the target version from the branch name
 $packageTargetVersion = $env:Build_SourceBranch
+
+# check if this is running from a checked out tag
+if ($packageTargetVersion -notlike "refs/tags/*") {
+    throw "ERROR: Branch name is not a tag! Either provide the version or checkout a tag before calling."
+}
+
 $packageTargetVersion = $packageTargetVersion -replace "refs/tags/", ""
-$packageTargetVersion = $packageTargetVersion.Substring(1)
+$packageTargetVersion = $packageTargetVersion -replace "^v"
+
+if ($packageTargetVersion -match "preview") {
+    # switch to develop branch for preview versions
+    $repoBranch = "develop"
+}
 
 # working directory is agent temp directory
 Write-Debug "Changing working directory to $env:Agent_TempDirectory"
@@ -56,15 +67,19 @@ Set-Location "$env:Agent_TempDirectory" | Out-Null
 # clone repo and checkout develop branch
 Write-Debug "Init and featch nf-Visual-Studio-extension repo"
 
-git clone --depth 1 https://github.com/nanoframework/nf-Visual-Studio-extension repo
+git clone --depth 1 --branch $repoBranch https://github.com/nanoframework/nf-Visual-Studio-extension repo
+
+if ($LASTEXITCODE -ne 0) {
+    throw "ERROR: Failed to clone branch '$repoBranch' from nf-Visual-Studio-extension."
+}
+
 Set-Location repo | Out-Null
 git config --global gc.auto 0
 git config --global user.name nfbot
 git config --global user.email nanoframework@outlook.com
 git config --global core.autocrlf true
 
-Write-Host "Checkout $repoMainBranch branch..."
-git checkout --quiet $repoMainBranch | Out-Null
+Write-Host "Checked out $repoBranch branch."
 
 # check if nuget package is already available from nuget.org
 $nugetApiUrl = "https://api.nuget.org/v3-flatcontainer/$packageName/index.json"
@@ -75,17 +90,31 @@ function Get-LatestNugetVersion {
     )
     try {
         $response = Invoke-RestMethod -Uri $url -Method Get
-        return $response.versions[-1]
+
+        if ($packageTargetVersion -match "preview") {
+            # Select only versions that include 'preview'
+            $versions = $response.versions | Where-Object { $_ -match "preview" }
+        }
+        else {
+            # Exclude any version that includes 'preview'
+            $versions = $response.versions | Where-Object { $_ -notmatch "preview" }
+        }
+
+        Write-Debug "Latest version found: $($versions[-1])"
+
+        return $versions[-1]
     }
     catch {
         throw "Error querying NuGet API: $_"
     }
 }
 
+Write-Host "Target version is: $packageTargetVersion."
+
 $latestNugetVersion = Get-LatestNugetVersion -url $nugetApiUrl
 
 while ($latestNugetVersion -ne $packageTargetVersion) {
-    Write-Host "Latest version still not available from nuget.org feed. Waiting 5 minutes..."
+    Write-Host "Target version ($packageTargetVersion) still not available from nuget.org feed. Waiting 5 minutes..."
     Start-Sleep -Seconds 300
     $latestNugetVersion = Get-LatestNugetVersion -url $nugetApiUrl
 }
@@ -164,9 +193,8 @@ if ($repoStatus -ne "")
     git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
 
     # start PR
-    # we are pointing to the $repoMainBranch 
-    # considering that the base branch can be changed at the PR ther is no big deal about this 
-    $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="$repoMainBranch"} | ConvertTo-Json
+    # considering that the base branch can be changed at the PR there is no big deal about this
+    $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="$repoBranch"} | ConvertTo-Json
     $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nf-Visual-Studio-extension/pulls"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
